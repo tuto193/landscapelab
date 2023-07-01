906cad922c68a1ea319bae0ccb812e52e32e5a01
Carlos Alfonso Parra Fernandez <carlos.a.parra.f@gmail.com>
2023-07-01 16:26:21 +0200

    fix: :bug: Fix call to WebSocketPeer.connect_to_url
    WebSocketPeer now requires TLSOptions.client(_unsafe) argument

--- addons/godot-websocket-nodes/WebSocketClient.gd
+++ addons/godot-websocket-nodes/WebSocketClient.gd
@@ -19,7 +19,11 @@ signal message_received(message: Variant)
 func connect_to_url(url) -> int:
 	socket.supported_protocols = supported_protocols
 	socket.handshake_headers = handshake_headers
-	var err = socket.connect_to_url(url, tls_verify, tls_trusted_certificate)
+	var tsl_options: TLSOptions = (
+			TLSOptions.client(tls_trusted_certificate) if tls_verify 
+			else TLSOptions.client_unsafe(tls_trusted_certificate)
+	)
+	var err = socket.connect_to_url(url, tsl_options)
 	if err != OK:
 		return err
 	last_state = socket.get_ready_state()
